b08b27f2373af0115f189619b295c8c8d30c702e
MatejTuray <41123694+MatejTuray@users.noreply.github.com>
2018-08-17 18:00:29 +0200

    header text"

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => {
      return (
         
         <div className="header">
-            <h1 className="header__text">This is KryptoTracker!</h1>
+            <h1 className="header__text">This is KryptoTracker</h1>
                 <p className="lead">Track your favorite cryptocurrencies in real-time</p>
                 <h3 className="subtext">You are currently tracking: {props.length} coin(s).  {props.length === 0 ? `Add one to get started!`: ""} </h3>
         </div>
